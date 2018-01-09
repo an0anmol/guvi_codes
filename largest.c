ccf9207377e1350eb659d5bba8009bfbf7b0587f
ano_anmol <anmolshukla7@gmail.com>
2018-01-09 22:11:46 +0530

    largest
    Largest amon three

---
+++ largest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if(a>b && a>c)
+{printf("%d",a);
+}else if(b>c && b>a)
+{printf("%d",b);
+}else if(c>b && c>a)
+{printf("%d",c);
+}
+
+}
